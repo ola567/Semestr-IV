7e3858f0604f467519a7d547a807fd7dcc3d1dea
Aleksandra Hein <76129631+ola567@users.noreply.github.com>
2022-04-07 21:09:06 +0200

    Add files via upload
    Pisanie testów

---
+++ Platformy_technologiczne/Lab_5/Mage.java
@@ -0,0 +1,25 @@
+public class Mage
+{
+    private String name;
+    private int level;
+
+    public Mage(String name, int level)
+    {
+        this.name = name;
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    @Override
+    public String toString()
+    {
+        return name;
+    }
+}
---
+++ Platformy_technologiczne/Lab_5/MageDto.java
@@ -0,0 +1,18 @@
+public class MageDto
+{
+    private String name;
+    private int level;
+
+    public MageDto(String name, int level) {
+        this.name = name;
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+}
